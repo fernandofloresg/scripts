84756047e08d3955460d6f037315af624dcbc512
Fernando Flores <fernandofloresdev@gmail.com>
2023-11-21 19:09:43 -0600

    Cambie operacion

--- mutliplica.py
+++ mutliplica.py
@@ -8,7 +8,7 @@ Created on Tue Nov 21 18:10:19 2023
 def multiplica(a,b): 
     variable1=a*2
     variable2=b*3
-    resultado=variable1*variable2
+    resultado=variable1 + variable2
     print("Empezando for ")
     for i in range(1, 5): 
         print(i)
